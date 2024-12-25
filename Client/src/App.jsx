b4a87243638573e13501d2ac99944f747c64f868
Nitin Gayke <gaykenitin975@gmail.com>
2024-12-25 23:15:00 +0530

    initialize repo

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl'>My Story</h1>
+
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
